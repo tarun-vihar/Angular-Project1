5cb64b7a619256c357042a0abacfde3f8af2a14e
unknown <Student@IT-STULOAN-523.csun.edu>
2021-11-05 11:47:09 -0700

    added heroku dependencies

---
+++ src/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static("./dist/hello-world"));
+
+app.get("/*", (req, res) =>
+  res.sendFile("index.html", { root: "dist/hello-world/" })
+);
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
