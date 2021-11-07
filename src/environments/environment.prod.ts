dfa67e5c1c944f917a71f3cfdf93fe41e142fe32
unknown <Student@IT-STULOAN-523.csun.edu>
2021-11-07 00:30:09 -0700

    configured temperory producation url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://localhost:8080/',
+  baseUrl: 'http://localhost:13000/',
   productAPI: 'https://fakestoreapi.com/products/',
 };
